8d62b4aacb8352693d61c7bf03f3d84c64f67f38
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-03 16:13:14 +0700

    update controllers

---
+++ lib/module/auth/controllers/signin/signin_phone_controller.dart
@@ -0,0 +1,19 @@
+// import 'dart:convert';
+import 'package:pasaraja_mobile/module/auth/models/user_model.dart';
+// import 'package:http/http.dart' as http;
+
+class SignInPhoneController {
+  static Future<UserModel> isExistPhone({
+    required String? phone,
+  }) async {
+    return const UserModel();
+  }
+
+  static Future<UserModel> loginPhone({
+    required String? phone,
+    required String? pin,
+  }) async {
+    // TODO
+    return const UserModel();
+  }
+}
