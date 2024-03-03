fe4d32225e804b50d25cbd56d78a7d3d1aa4d04b
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-03 16:12:30 +0700

    update config cores

---
+++ lib/core/services/google_signin_services.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:logger/logger.dart';
+
+class GoogleSignService extends ChangeNotifier {
+  final googleSignIn = GoogleSignIn();
+
+  GoogleSignInAccount? _user;
+
+   GoogleSignInAccount get user => _user!;
+
+  /// sign in as anonymous
+  Future<void> signInAsAnonymous() async {
+    try {
+      final credential = await FirebaseAuth.instance.signInAnonymously();
+      Logger().i(
+          'Signed in with anonymous account -> UID : ${credential.user?.uid}');
+    } catch (ex) {
+      Logger().e(ex.toString());
+    }
+  }
+
+
+  Future googleLogin() async {
+    try {
+      final googleUser = await googleSignIn.signIn();
+      if (googleUser == null) return;
+      _user = googleUser;
+
+      final googleAuth = await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      Logger().e(e);
+    }
+
+    notifyListeners();
+  }
+
+  Future logout() async {
+    await googleSignIn.disconnect();
+    FirebaseAuth.instance.signOut();
+  }
+}
