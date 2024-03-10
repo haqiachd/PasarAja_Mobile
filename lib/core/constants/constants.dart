1a14310ebddc1aaf748fa9ffa099a2ce98d458e0
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-11 05:22:53 +0700

    add dio exception log

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -3,7 +3,8 @@ class PasarAjaConstant {
   static const String rights = 'Copyright © 2024. TIF 22 Kelompok C1.';
   static const double authTolbarHeight = 40;
   static const int initLoading = 3;
-  static const String baseUrl = 'http://192.168.1.152:8000/api/m';
+  static const String baseUrl = 'http://192.168.170.152:8000/api/m';
   static const String npeString = 'Exception : NPE';
   static const String unknownError = 'unknown error';
+  static const Duration transitionDuration = Duration(milliseconds: 800);
 }
--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:vibration/vibration.dart';
 
 class PasarAjaUtils {
@@ -15,5 +17,33 @@ class PasarAjaUtils {
     } else {
       print('Perangkat tidak mendukung getaran.');
     }
+  }
+
+  static showDioException(
+    Dio dio, {
+    bool responseData = false,
+    bool requestData = false,
+    bool responseHeaders = false,
+    bool requestHeaders = false,
+    bool responseMessage = false,
+  }) {
+    dio.interceptors.add(
+      TalkerDioLogger(
+        settings: TalkerDioLoggerSettings(
+          // All http request & responses enabled for console logging
+          printResponseData: responseData,
+          printRequestData: requestData,
+          // Request & Reposnse logs including http - headers
+          printResponseHeaders: responseHeaders,
+          printRequestHeaders: requestHeaders,
+          // Request logs with response message
+          printResponseMessage: responseMessage,
+        ),
+      ),
+    );
+  }
+
+  static clearDioException(Dio dio) {
+    // dio.interceptors.clear();
   }
 }
