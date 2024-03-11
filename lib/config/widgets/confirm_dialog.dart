9da20faf54769926eb56e8a2fba9ec591a888bf3
Achmad Baihaqi <haqiachd@gmail.com>
2024-03-11 19:35:10 +0700

    add confirm dialog

---
+++ lib/config/widgets/confirm_dialog.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ConfirmDialog extends StatelessWidget {
+  final String title;
+  final String message;
+
+  const ConfirmDialog({Key? key, required this.title, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            Get.back(result: false);
+          },
+          child: Text('Tidak'),
+        ),
+        TextButton(
+          onPressed: () {
+            Get.back(result: true);
+          },
+          child: Text('Ya'),
+        ),
+      ],
+    );
+  }
+}
