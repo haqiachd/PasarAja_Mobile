bab017c30c62b2540a5039a2628f1940e842cedc
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-01 01:51:30 +0700

    update on/off stok

--- lib/module/merchant/providers/product/unavailable_provider.dart
+++ lib/module/merchant/providers/product/unavailable_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:pasaraja_mobile/core/sources/data_state.dart';
 import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+import 'package:pasaraja_mobile/core/utils/messages.dart';
 import 'package:pasaraja_mobile/module/merchant/controllers/product_controller.dart';
 import 'package:pasaraja_mobile/module/merchant/models/product_model.dart';
 
@@ -16,6 +18,13 @@ class UnavailableProvider extends ChangeNotifier {
   List<ProductModel> _unavailables = [];
   List<ProductModel> get unavailables => _unavailables;
 
+  bool _isAvailable = false;
+  bool get isAvailable => _isAvailable;
+  set isAvailable(bool value) {
+    _isAvailable = value;
+    notifyListeners();
+  }
+
   Future<void> fetchData() async {
     try {
       // set loading
@@ -29,6 +38,7 @@ class UnavailableProvider extends ChangeNotifier {
 
       // response is successful
       if (dataState is DataSuccess) {
+        _isAvailable = false;
         _unavailables = dataState.data as List<ProductModel>;
         state = const OnSuccessState();
       }
@@ -42,6 +52,34 @@ class UnavailableProvider extends ChangeNotifier {
     } catch (ex) {
       state = OnFailureState(message: ex.toString());
       notifyListeners();
+    }
+  }
+
+  Future<void> updateAvailable({required int idProduct}) async {
+    try {
+      Get.back();
+      // show loading
+      PasarAjaMessage.showLoading();
+
+      final dataState = await _prodController.updateSettingStok(
+        idShop: 1,
+        idProduct: idProduct,
+        stokStatus: isAvailable,
+      );
+
+      Get.back();
+
+      if (dataState is DataSuccess) {
+        await PasarAjaMessage.showInformation("Stok berhasil diupdate");
+      }
+
+      if (dataState is DataFailed) {
+        await PasarAjaMessage.showInformation(
+          "Stok gagal diupdate!\n\nError : ${dataState.error?.error}",
+        );
+      }
+    } catch (ex) {
+      // nothing
     }
   }
 }
--- lib/module/merchant/views/product/unavailable_page.dart
+++ lib/module/merchant/views/product/unavailable_page.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pasaraja_mobile/config/themes/typography.dart';
 import 'package:pasaraja_mobile/config/widgets/loading_indicator.dart';
 import 'package:pasaraja_mobile/config/widgets/merchant_sub_appbar.dart';
 import 'package:pasaraja_mobile/config/widgets/page_error_message.dart';
 import 'package:pasaraja_mobile/config/widgets/something_wrong.dart';
 import 'package:pasaraja_mobile/core/constants/constants.dart';
 import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+import 'package:pasaraja_mobile/module/merchant/models/product_model.dart';
 import 'package:pasaraja_mobile/module/merchant/providers/product/unavailable_provider.dart';
+import 'package:pasaraja_mobile/module/merchant/widgets/action_button.dart';
 import 'package:pasaraja_mobile/module/merchant/widgets/item_unavailable.dart';
+import 'package:pasaraja_mobile/module/merchant/widgets/switcher_setting.dart';
 import 'package:provider/provider.dart';
 
 class UnavailablePage extends StatefulWidget {
@@ -67,8 +71,15 @@ class UnavailablePageState extends State<UnavailablePage> {
               return ListView.builder(
                 itemCount: value.unavailables.length,
                 itemBuilder: (context, index) {
+                  final ProductModel data = value.unavailables[index];
                   return ItemUnavailable(
-                    product: value.unavailables[index],
+                    product: data,
+                    onTap: () {
+                      _showBottomSheet(
+                        context,
+                        data.id ?? 0,
+                      );
+                    },
                   );
                 },
               );
@@ -78,6 +89,54 @@ class UnavailablePageState extends State<UnavailablePage> {
           },
         ),
       ),
+    );
+  }
+
+  _showBottomSheet(BuildContext context, int idProduct) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.white,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Update Stok',
+                style: PasarAjaTypography.sfpdBold.copyWith(
+                  fontSize: 22,
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Consumer<UnavailableProvider>(
+                builder: (context, prov, child) {
+                  return SwitcherSetting(
+                    title: 'Ketersediaan Stok',
+                    description: 'Update ketersediaan stok dari produk',
+                    value: prov.isAvailable,
+                    onChanged: (value) {
+                      context.read<UnavailableProvider>().isAvailable = value;
+                    },
+                  );
+                },
+              ),
+              const SizedBox(height: 40.0),
+              ActionButton(
+                title: 'Simpan',
+                state: ActionButton.stateEnabledButton,
+                onPressed: () async {
+                  await context.read<UnavailableProvider>().updateAvailable(
+                        idProduct: idProduct,
+                      );
+                  await _fetchData();
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
