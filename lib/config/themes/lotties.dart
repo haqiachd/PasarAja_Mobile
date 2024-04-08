01d13c31428695df72dcf60733de3fd1f26a1fac
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-09 02:05:46 +0700

    add lottie assets

---
+++ lib/config/themes/lotties.dart
@@ -0,0 +1,6 @@
+class PasarAjaLottie {
+  static const String _dir = 'assets/raws/';
+  static const String notFound1 = '${_dir}data_not_found_1.json';
+  static const String notFound2 = '${_dir}data_not_found_2.json';
+  static const String notFound3 = '${_dir}data_not_found_3.json';
+}
