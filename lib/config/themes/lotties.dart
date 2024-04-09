c081f7c2ad981e6daf1de513f66a23af80901309
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-09 22:26:48 +0700

    update lottie assets

--- lib/config/themes/lotties.dart
+++ lib/config/themes/lotties.dart
@@ -3,4 +3,7 @@ class PasarAjaLottie {
   static const String notFound1 = '${_dir}data_not_found_1.json';
   static const String notFound2 = '${_dir}data_not_found_2.json';
   static const String notFound3 = '${_dir}data_not_found_3.json';
+  static const String noOrder = '${_dir}no_order.json';
+  static const String error = '${_dir}error.json';
+  static const String somethingWrong = '${_dir}something_wrong.json';
 }
