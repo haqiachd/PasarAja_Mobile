6b5c6bf950fbe50fdc880c13aad425ce205260ac
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-12 14:49:13 +0700

    update order providers

--- lib/module/merchant/views/order/order_expired_tab.dart
+++ lib/module/merchant/views/order/order_expired_tab.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:pasaraja_mobile/config/themes/colors.dart';
+import 'package:get/get.dart';
 import 'package:pasaraja_mobile/config/themes/typography.dart';
 import 'package:pasaraja_mobile/config/widgets/loading_indicator.dart';
 import 'package:pasaraja_mobile/config/widgets/page_error_message.dart';
 import 'package:pasaraja_mobile/config/widgets/something_wrong.dart';
 import 'package:pasaraja_mobile/core/constants/constants.dart';
 import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+import 'package:pasaraja_mobile/core/utils/utils.dart';
 import 'package:pasaraja_mobile/module/merchant/models/transaction_model.dart';
 import 'package:pasaraja_mobile/module/merchant/providers/providers.dart';
+import 'package:pasaraja_mobile/module/merchant/views/order/order_detail_page.dart';
 import 'package:pasaraja_mobile/module/merchant/widgets/empty_order.dart';
 import 'package:provider/provider.dart';
 
@@ -61,22 +63,12 @@ class _OrderExpiredTabState extends State<OrderExpiredTab> {
           if (order.state is OnSuccessState) {
             List<TransactionModel> orders = order.orders;
             if (orders.isNotEmpty) {
-              return ListView(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height / 2,
-                    child: Center(
-                      child: Text(
-                        'Order Expired',
-                        style: PasarAjaTypography.sfpdBold.copyWith(
-                          fontSize: 25,
-                          color: PasarAjaColor.green1,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+              return ListView.builder(
+                itemCount: orders.length,
+                itemBuilder: (context, index) {
+                  TransactionModel order = orders[index];
+                  return _itemOrder(order);
+                },
               );
             } else {
               return const EmptyOrder("Belum ada Pesanan yang Kadaluarsa");
@@ -85,6 +77,74 @@ class _OrderExpiredTabState extends State<OrderExpiredTab> {
 
           return const SomethingWrong();
         },
+      ),
+    );
+  }
+
+  _itemOrder(TransactionModel order) {
+    return InkWell(
+      onTap: () {
+        Get.to(
+          OrderDetailPage(orderCode: order.orderCode ?? ''),
+          transition: Transition.cupertino,
+          duration: PasarAjaConstant.transitionDuration,
+        );
+      },
+      child: ListTile(
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              order.orderId ?? '',
+              style: PasarAjaTypography.sfpdBold.copyWith(
+                color: Colors.blue,
+                fontSize: 22,
+              ),
+            ),
+            Text(
+              order.fullName ?? '',
+              style: PasarAjaTypography.sfpdBold,
+            ),
+            Text(
+              "${order.details?.length ?? 0} x product",
+              style: PasarAjaTypography.sfpdSemibold,
+            ),
+            const Text("_"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: order.details!
+                  .map(
+                    (prod) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${prod.quantity} x ${prod.productName}",
+                          style: PasarAjaTypography.sfpdRegular,
+                        ),
+                        Text(
+                          "Rp. ${PasarAjaUtils.formatPrice(prod.price ?? 0)}",
+                          style: PasarAjaTypography.sfpdRegular,
+                        ),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
+            const Text("_"),
+            Text(
+              "${order.totalQuantity} Produk",
+              style: PasarAjaTypography.sfpdBold,
+            ),
+            Text(
+              "Rp. ${PasarAjaUtils.formatPrice(order.subTotal ?? 0)}",
+              style: PasarAjaTypography.sfpdBold,
+            ),
+            const Divider(),
+          ],
+        ),
       ),
     );
   }
