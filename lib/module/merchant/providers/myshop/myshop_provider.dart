fef2738072b01f1e3adac1e57706c6c50d1c76ab
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-19 20:55:32 +0700

    add provider myshop

--- lib/module/merchant/providers/myshop/myshop_provider.dart
+++ lib/module/merchant/providers/myshop/myshop_provider.dart
@@ -1,3 +1,31 @@
-class MyShopProvider {
-  //
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:pasaraja_mobile/config/themes/images.dart';
+import 'package:pasaraja_mobile/core/services/user_services.dart';
+import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+
+class MyShopProvider extends ChangeNotifier{
+  ProviderState state = const OnInitState();
+
+  String _photoProfile = '';
+  String get photoProfile => _photoProfile;
+  set photoProfile(String f){
+    _photoProfile = f;
+    notifyListeners();
+  }
+
+  Future<void> fetchData() async {
+    try{
+      state = const OnLoadingState();
+      notifyListeners();
+
+      _photoProfile = await PasarAjaUserService.getUserData(PasarAjaUserService.photo);
+
+      notifyListeners();
+    }catch(ex){
+      state = OnFailureState(message: ex.toString());
+      notifyListeners();
+    }
+  }
 }
--- lib/module/merchant/views/myshop/myshop_page.dart
+++ lib/module/merchant/views/myshop/myshop_page.dart
@@ -3,8 +3,10 @@ import 'package:get/get.dart';
 import 'package:pasaraja_mobile/config/themes/colors.dart';
 import 'package:pasaraja_mobile/config/widgets/app_bar.dart';
 import 'package:pasaraja_mobile/config/widgets/coming_soon.dart';
+import 'package:pasaraja_mobile/module/merchant/providers/myshop/myshop_provider.dart';
 import 'package:pasaraja_mobile/module/merchant/views/myshop/profile_page.dart';
 import 'package:pasaraja_mobile/module/merchant/widgets/photo_profile.dart';
+import 'package:provider/provider.dart';
 
 class MyShopPage extends StatefulWidget {
   const MyShopPage({super.key});
@@ -14,25 +16,52 @@ class MyShopPage extends StatefulWidget {
 }
 
 class _MyShopPageState extends State<MyShopPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async{
+      await context.read<MyShopProvider>().fetchData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: PasarAjaColor.white,
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: PasarAjaAppbar(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          97 - MediaQuery.of(context).padding.top,
+        ),
+        child: Consumer<MyShopProvider>(
+          builder: (context, provider, child) {
+            return PasarAjaAppbar(
               title: 'Toko Saya',
               action: PhotoProfile(
+                photoPath: provider.photoProfile,
                 onTap: () {
                   Get.to(const ProfilePage());
                 },
               ),
-            ),
-          ),
+            );
+          },
+        ),
+      ),
+      body: Stack(
+        children: [
+          // Positioned(
+          //   top: 0,
+          //   left: 0,
+          //   right: 0,
+          //   child: PasarAjaAppbar(
+          //     title: 'Toko Saya',
+          //     action: PhotoProfile(
+          //       onTap: () {
+          //         Get.to(const ProfilePage());
+          //       },
+          //     ),
+          //   ),
+          // ),
           const ComingSoon(),
         ],
       ),
