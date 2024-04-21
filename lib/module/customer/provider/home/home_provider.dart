b6fa96313e660bf4112eb47cb62cfb4600870ba3
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-21 22:50:25 +0700

    update view & provider for home

--- lib/module/customer/provider/home/home_provider.dart
+++ lib/module/customer/provider/home/home_provider.dart
@@ -1,3 +1,28 @@
-class HomeProvider{
-  //
+import 'package:flutter/material.dart';
+import 'package:pasaraja_mobile/core/services/user_services.dart';
+import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+
+class HomeProvider extends ChangeNotifier{
+  ProviderState state = const OnInitState();
+
+  String _photoProfile = '';
+  String get photoProfile => _photoProfile;
+  set photoProfile(String f){
+    _photoProfile = f;
+    notifyListeners();
+  }
+
+  Future<void> fetchData() async {
+    try{
+      state = const OnLoadingState();
+      notifyListeners();
+
+      _photoProfile = await PasarAjaUserService.getUserData(PasarAjaUserService.photo);
+
+      notifyListeners();
+    }catch(ex){
+      state = OnFailureState(message: ex.toString());
+      notifyListeners();
+    }
+  }
 }
--- lib/module/customer/views/home/home_page.dart
+++ lib/module/customer/views/home/home_page.dart
@@ -3,8 +3,10 @@ import 'package:get/get.dart';
 import 'package:pasaraja_mobile/config/themes/colors.dart';
 import 'package:pasaraja_mobile/config/widgets/app_bar.dart';
 import 'package:pasaraja_mobile/config/widgets/coming_soon.dart';
+import 'package:pasaraja_mobile/module/customer/provider/home/home_provider.dart';
 import 'package:pasaraja_mobile/module/customer/views/home/profile_page.dart';
 import 'package:pasaraja_mobile/module/customer/widgets/photo_profile.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,25 +16,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async{
+      await context.read<HomeProvider>().fetchData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: PasarAjaColor.white,
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: PasarAjaAppbar(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(
+          97 - MediaQuery.of(context).padding.top,
+        ),
+        child: Consumer<HomeProvider>(
+          builder: (context, provider, child) {
+            return PasarAjaAppbar(
               title: 'Beranda',
               action: PhotoProfile(
+                photoPath: provider.photoProfile,
                 onTap: () {
                   Get.to(const ProfilePage());
                 },
               ),
-            ),
-          ),
+            );
+          },
+        ),
+      ),
+      body: Stack(
+        children: [
+          // Positioned(
+          //   top: 0,
+          //   left: 0,
+          //   right: 0,
+          //   child: PasarAjaAppbar(
+          //     title: 'Toko Saya',
+          //     action: PhotoProfile(
+          //       onTap: () {
+          //         Get.to(const ProfilePage());
+          //       },
+          //     ),
+          //   ),
+          // ),
           const ComingSoon(),
         ],
       ),
