54761bf6635b3331c2c78d9efff3666d000af9d8
Achmad Baihaqi <haqiachd@gmail.com>
2024-04-30 23:35:31 +0700

    add tab bar

--- lib/module/customer/views/shopping/shopping_page.dart
+++ lib/module/customer/views/shopping/shopping_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pasaraja_mobile/config/themes/Typography.dart';
 import 'package:pasaraja_mobile/config/themes/colors.dart';
 import 'package:pasaraja_mobile/config/widgets/app_bar.dart';
-import 'package:pasaraja_mobile/config/widgets/coming_soon.dart';
+import 'package:pasaraja_mobile/module/customer/views/shopping/product_tab.dart';
+import 'package:pasaraja_mobile/module/customer/views/shopping/shop_tab.dart';
 
 class ShoppingPage extends StatefulWidget {
   const ShoppingPage({super.key});
@@ -13,20 +15,52 @@ class ShoppingPage extends StatefulWidget {
 class _ShoppingPageState extends State<ShoppingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: PasarAjaColor.white,
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: PasarAjaAppbar(
-              title: 'Belanja',
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(97 - MediaQuery.of(context).padding.top),
+        child: const PasarAjaAppbar(
+          title: 'Belanja',
+        ),
+      ),
+      body: DefaultTabController(
+        length: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 16),
+              child: TabBar(
+                labelStyle: PasarAjaTypography.sfpdBold,
+                labelColor: Colors.black,
+                unselectedLabelColor: PasarAjaColor.gray5,
+                indicatorColor: PasarAjaColor.green1,
+                indicatorWeight: 4,
+                isScrollable: true,
+                tabAlignment: TabAlignment.start,
+                tabs: const [
+                  Tab(text: 'Toko'),
+                  Tab(text: 'Produk'),
+                ],
+              ),
             ),
-          ),
-          ComingSoon(),
-        ],
+            Expanded(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: const TabBarView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      ShopTab(),
+                      ProductTab(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
