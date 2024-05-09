3243c491f76f08f1041e396044b758ad1948a992
Achmad Baihaqi <haqiachd@gmail.com>
2024-05-09 21:35:04 +0700

    register assets in config

--- lib/config/themes/icons.dart
+++ lib/config/themes/icons.dart
@@ -7,4 +7,15 @@ class PasarAjaIcon {
   static const String icNumber = '${_dir}ic-number.svg';
   static const String icClearText = '${_dir}ic-clear-text.svg';
   static const String icLineIndicator = '${_dir}ic-sheet-indicator.svg';
+  static final String gofood = '${_dir}ic-gofood.svg';
+  static final String gomart = '${_dir}ic-gomart.svg';
+  static final String gopayCircle = '${_dir}ic-gopay-circle.svg';
+  static final String gopayMenuBayar = '${_dir}ic-gopay-menu-bayar.svg';
+  static final String gopayMenuOthers = '${_dir}ic-gopay-menu-others.svg';
+  static final String gopayMenuTopup = '${_dir}ic-gopay-menu-topup.svg';
+  static final String gopay = '${_dir}ic-gopay.svg';
+  static final String others = '${_dir}ic-others.svg';
+  static final String rating = '${_dir}ic-rating.svg';
+  static final String search = '${_dir}ic-search.svg';
+  static final String tokopedia = '${_dir}ic-tokopedia.svg';
 }
--- lib/config/themes/typography.dart
+++ lib/config/themes/typography.dart
@@ -64,4 +64,73 @@ class PasarAjaTypography {
     fontSize: 30,
     letterSpacing: 1.5,
   );
+
+  /// SF Pro Display Font
+  static final TextStyle primary = TextStyle(
+    fontFamily: 'SF-Pro-Display',
+  );
+
+  /// SF Pro Display Regular 12.5
+  static final TextStyle regular12_5 = primary.copyWith(
+    fontSize: 12.5,
+  );
+
+  /// SF Pro Display Regular 14
+  static final TextStyle regular14 = regular12_5.copyWith(
+    fontSize: 14,
+    letterSpacing: 0.4,
+  );
+
+  /// SF Pro Display Regular 14
+  static final TextStyle regular14_line_spaccing_140 = regular12_5.copyWith(
+    fontSize: 14.4,
+    letterSpacing: 0.4,
+  );
+
+  /// SF Pro Display Semibold 11.5
+  static final TextStyle semibold11_5 = primary.copyWith(
+    fontSize: 11.5,
+    fontWeight: FontWeight.w600,
+  );
+
+  /// SF Pro Display Semibold 12.5
+  static final TextStyle semibold12_5 = semibold11_5.copyWith(
+    fontSize: 12.5,
+    fontWeight: FontWeight.w600,
+  );
+
+  /// SF Pro Display Semibold 14
+  static final TextStyle semibold14 = semibold12_5.copyWith(
+    fontSize: 14,
+    letterSpacing: 0.1,
+  );
+
+  /// SF Pro Display Bold 14
+  static final TextStyle bold14 = regular12_5.copyWith(
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+    letterSpacing: 0.1,
+  );
+
+  /// SF Pro Display Bold 16
+  static final TextStyle bold16 = bold14.copyWith(
+    fontSize: 16,
+  );
+
+  /// SF Pro Display Bold 16
+  static final TextStyle bold16_letter_spacing = bold16.copyWith(
+    fontWeight: FontWeight.w700,
+  );
+
+  /// SF Pro Display Bold 18
+  static TextStyle bold18 = bold16.copyWith(
+    fontSize: 18,
+    letterSpacing: -0.5,
+  );
+
+  /// SF Pro Display Bold 24
+  static final TextStyle bold24 = bold18.copyWith(
+    fontSize: 24,
+    letterSpacing: -0.2,
+  );
 }
