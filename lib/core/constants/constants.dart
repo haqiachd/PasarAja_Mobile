1023bc9292eb93f34088829f6a3d9eb12c4bb625
Achmad Baihaqi <haqiachd@gmail.com>
2024-05-23 03:16:51 +0700

    update base url

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -4,7 +4,7 @@ class PasarAjaConstant {
   static const double authTolbarHeight = 40;
   static const int initLoading = 3;
   /// http://192.168.170.152:8000/api/m
-  static const String baseUrl = 'http://192.168.170.152:8000/api/m';
+  static const String baseUrl = 'http://192.168.6.152:8000/api/m';
   static const String npeString = 'Exception : NPE';
   static const String unknownError = 'unknown error';
   static const Duration transitionDuration = Duration(milliseconds: 800);
