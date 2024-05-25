1dca97b1632c078b234b335a7ebe5fb0cbb32978
Achmad Baihaqi <haqiachd@gmail.com>
2024-05-25 16:54:35 +0700

    add logic in edit shop operational

--- lib/module/merchant/providers/myshop/edit_operational_provider.dart
+++ lib/module/merchant/providers/myshop/edit_operational_provider.dart
@@ -1,5 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pasaraja_mobile/core/constants/constants.dart';
+import 'package:pasaraja_mobile/core/services/user_services.dart';
+import 'package:pasaraja_mobile/core/sources/data_state.dart';
+import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+import 'package:pasaraja_mobile/core/utils/messages.dart';
+import 'package:pasaraja_mobile/module/merchant/controllers/myshop_controller.dart';
+import 'package:pasaraja_mobile/module/merchant/models/operational_model.dart';
 
-class EditOperationalProvider extends ChangeNotifier{
-  //
+class EditOperationalProvider extends ChangeNotifier {
+  final _controller = MyShopController();
+
+  ProviderState state = const OnInitState();
+
+  bool _monday = false;
+  bool get monday => _monday;
+  set monday(bool s) {
+    _monday = s;
+    notifyListeners();
+  }
+
+  bool _tuesday = false;
+  bool get tuesday => _tuesday;
+  set tuesday(bool s) {
+    _tuesday = s;
+    notifyListeners();
+  }
+
+  bool _wednesday = false;
+  bool get wednesday => _wednesday;
+  set wednesday(bool s) {
+    _wednesday = s;
+    notifyListeners();
+  }
+
+  bool _thursday = false;
+  bool get thursday => _thursday;
+  set thursday(bool s) {
+    _thursday = s;
+    notifyListeners();
+  }
+
+  bool _friday = false;
+  bool get friday => _friday;
+  set friday(bool s) {
+    _friday = s;
+    notifyListeners();
+  }
+
+  bool _saturday = false;
+  bool get saturday => _saturday;
+  set saturday(bool s) {
+    _saturday = s;
+    notifyListeners();
+  }
+
+  bool _sunday = false;
+  bool get sunday => _sunday;
+  set sunday(bool s) {
+    _sunday = s;
+    notifyListeners();
+  }
+
+  Future<void> init() async {
+    try{
+      state = const OnLoadingState();
+      notifyListeners();
+
+      final idShop = await PasarAjaUserService.getShopId();
+
+      final dataState = await _controller.getOperational(
+        idShop: idShop,
+      );
+
+      if (dataState is DataSuccess) {
+
+        state = const OnSuccessState();
+        notifyListeners();
+
+        var jam = dataState.data as OperationalModel;
+
+        _monday = jam.senin ?? false;
+        _tuesday = jam.selasa ?? false;
+        _wednesday = jam.rabu ?? false;
+        _thursday = jam.kamis ?? false;
+        _friday = jam.jumat ?? false;
+        _saturday = jam.sabtu ?? false;
+        _sunday = jam.minggu ?? false;
+        notifyListeners();
+      }
+
+      if (dataState is DataFailed) {
+        state = OnFailureState(
+          dioException: dataState.error,
+        );
+        notifyListeners();
+      }
+    }catch(ex){
+      state = OnFailureState(
+        message: ex.toString(),
+      );
+      notifyListeners();
+    }
+  }
+
+  Future<void> onSaveButton() async {
+    try {
+      //
+      var jamBuka = OperationalModel(
+        senin: _monday,
+        selasa: _tuesday,
+        rabu: _wednesday,
+        kamis: _thursday,
+        jumat: _friday,
+        sabtu: _saturday,
+        minggu: _sunday,
+      );
+
+      await PasarAjaMessage.showLoading();
+
+      final idShop = await PasarAjaUserService.getShopId();
+
+      final dataState = await _controller.updateOperational(
+        idShop: idShop,
+        operational: jamBuka,
+      );
+
+      Get.back();
+
+      if (dataState is DataSuccess) {
+        await PasarAjaMessage.showInformation('Jam Buka Toko Berhasil Diedit');
+      }
+
+      if (dataState is DataFailed) {
+        await PasarAjaMessage.showSnackbarWarning(
+          dataState.error?.error?.toString() ?? PasarAjaConstant.unknownError,
+        );
+      }
+    } catch (ex) {
+      await PasarAjaMessage.showSnackbarWarning('Error : $ex');
+    }
+  }
 }
--- lib/module/merchant/views/myshop/edit_operational_page.dart
+++ lib/module/merchant/views/myshop/edit_operational_page.dart
@@ -1,25 +1,245 @@
+import 'package:d_method/d_method.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pasaraja_mobile/config/themes/Typography.dart';
+import 'package:pasaraja_mobile/config/widgets/action_button.dart';
+import 'package:pasaraja_mobile/config/widgets/loading_indicator.dart';
 import 'package:pasaraja_mobile/config/widgets/merchant_sub_appbar.dart';
+import 'package:pasaraja_mobile/config/widgets/page_error_message.dart';
+import 'package:pasaraja_mobile/config/widgets/something_wrong.dart';
+import 'package:pasaraja_mobile/core/sources/provider_state.dart';
+import 'package:pasaraja_mobile/module/merchant/providers/myshop/edit_operational_provider.dart';
+import 'package:provider/provider.dart';
 
 class EditOperationalPage extends StatefulWidget {
   const EditOperationalPage({super.key});
 
   @override
-  State<EditOperationalPage> createState() =>
-      _EditOperationalPageState();
+  State<EditOperationalPage> createState() => _EditOperationalPageState();
 }
 
 class _EditOperationalPageState extends State<EditOperationalPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      await context.read<EditOperationalProvider>().init();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: merchantSubAppbar('Edit Jam Buka'),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [],
-        ),
+      body: Consumer<EditOperationalProvider>(
+        builder: (context, provider, child) {
+          if (provider.state is OnLoadingState) {
+            return const LoadingIndicator();
+          }
+
+          if (provider.state is OnFailureState) {
+            return PageErrorMessage(
+                onFailureState: provider.state as OnFailureState);
+          }
+
+          if (provider.state is OnSuccessState) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Nama Hari',
+                          style: PasarAjaTypography.sfpdBold
+                              .copyWith(fontSize: 20),
+                        ),
+                        Text(
+                          'Status Buka',
+                          style: PasarAjaTypography.sfpdBold
+                              .copyWith(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const Divider(),
+                    _buildMonday(),
+                    _buildTuesday(),
+                    _buildWednesday(),
+                    _buildThursday(),
+                    _buildFriday(),
+                    _buildSaturday(),
+                    _buildSunday(),
+                    const SizedBox(height: 50),
+                    _buildSaveButton(),
+                  ],
+                ),
+              ),
+            );
+          }
+
+          return const SomethingWrong();
+        },
       ),
+    );
+  }
+
+  _buildSaveButton() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return ActionButton(
+          onPressed: () {
+            provider.onSaveButton();
+          },
+          title: 'Save',
+          state: ActionButton.stateEnabledButton,
+        );
+      },
+    );
+  }
+
+  _buildMonday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Senin',
+          isChecked: provider.monday,
+          onChanged: () {
+            DMethod.log('tessss');
+            provider.monday = !provider.monday;
+          },
+        );
+      },
+    );
+  }
+
+  _buildTuesday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Selasa',
+          isChecked: provider.tuesday,
+          onChanged: () {
+            provider.tuesday = !provider.tuesday;
+          },
+        );
+      },
+    );
+  }
+
+  _buildWednesday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Rabu',
+          isChecked: provider.wednesday,
+          onChanged: () {
+            provider.wednesday = !provider.wednesday;
+          },
+        );
+      },
+    );
+  }
+
+  _buildThursday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Kamis',
+          isChecked: provider.thursday,
+          onChanged: () {
+            provider.thursday = !provider.thursday;
+          },
+        );
+      },
+    );
+  }
+
+  _buildFriday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Jumat',
+          isChecked: provider.friday,
+          onChanged: () {
+            provider.friday = !provider.friday;
+          },
+        );
+      },
+    );
+  }
+
+  _buildSaturday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Sabtu',
+          isChecked: provider.saturday,
+          onChanged: () {
+            provider.saturday = !provider.saturday;
+          },
+        );
+      },
+    );
+  }
+
+  _buildSunday() {
+    return Consumer<EditOperationalProvider>(
+      builder: (context, provider, child) {
+        return JadwalWidget(
+          day: 'Minggu',
+          isChecked: provider.sunday,
+          onChanged: () {
+            provider.sunday = !provider.sunday;
+          },
+        );
+      },
+    );
+  }
+}
+
+class JadwalWidget extends StatelessWidget {
+  const JadwalWidget({
+    super.key,
+    required this.day,
+    required this.isChecked,
+    required this.onChanged,
+  });
+
+  final String day;
+  final bool isChecked;
+  final Function onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              day,
+              style: PasarAjaTypography.sfpdBold.copyWith(fontSize: 20),
+            ),
+            Switch(
+              value: isChecked,
+              onChanged: (b) {
+                onChanged();
+              },
+            ),
+          ],
+        ),
+        const Divider(),
+      ],
     );
   }
 }
